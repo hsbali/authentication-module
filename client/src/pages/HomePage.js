7836c31a3985a7f232c8cfd05335d71b0395d71b
Harmeet Singh <hsbaliwork@gmail.com>
2022-04-08 23:08:18 +0530

    added surprise

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,27 @@
 import { useDispatch, useSelector } from "react-redux"
+import useWindowSize from "../hooks/useWindowSize";
+import Confetti from 'react-confetti'
 import { Link } from "react-router-dom";
 
 import { logout } from "../store/actions/auth.action";
 
 export default function HomePage() {
     const dispatch = useDispatch()
+    const { width, height } = useWindowSize();
 
+    const userAuthState = useSelector((state) => state.auth.isAuthenticated)
     const userData = useSelector((state) => state.auth.user);
 
     return (
         <>
+            {userAuthState && <Confetti
+                width={width}
+                height={height}
+                numberOfPieces={700}
+                recycle={false}
+                friction={1.01}
+            />}
+
             <main className="container mx-auto p-4">
                 <section className="my-8 text-center">
 
